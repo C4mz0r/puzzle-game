41ae30d38ce0b0f381e62eb34cbf32697c492228
C4mz0r <aut0x@hotmail.com>
2015-08-15 00:43:12 -0600

    add shuffling, hints and fixed bug with non-adjacent edges swapping

--- game.js
+++ game.js
@@ -1,55 +1,64 @@
+var hintsOn = false;  // used for toggling hints on and off
+
 $(document).ready (
 
 
 	function()
 	{
+
+		// Shuffle the grid if clicked.
+		$("#shuffle").click( function() {
+
+			for (var i=0; i<= 1000; i++)
+			{
+				tryToSwapTiles();
+			}			
+		});
+
+		// Toggle hints (numbers) on and off
+		$("#hint").click(
+			function(){
+				hintsOn = !hintsOn;
+				updateText();
+			}
+		);
+
+
+		// Swap tiles around if allowable
 		$("div").click(
 			function() {
-				//alert($(this).hasClass());
-				//$(this).addClass("empty");
 				if ( $(this).hasClass("container") == false && $(this).hasClass("row") == false  )
-				{
-					
-					//alert("is it div 1? " + $(this).is("#div1"));
-					//alert("is 
-//					alert(isCellNextToEmpty($(this)).length);
+				{										
 					if ( adjacents = isCellNextToEmpty($(this)) ) // swap
 					{
-						//alert(adjacents.length);
 						if (adjacents.length == 2 )
 						{
 							// swap clicked and empty						
-							var adjacentClass = $("#div"+adjacents[0]).attr('class'); //getAdjacentClass( adjacents[0] );  // find out what class div+adjacents[0] has...
-							//alert('adj' + adjacentClass);
+							var adjacentClass = $("#div"+adjacents[0]).attr('class');
 							$("#div"+adjacents[1]).addClass( adjacentClass ).removeClass("empty");
-							$("#div"+adjacents[0]).addClass("empty").removeClass("div1 div2 div3 div4 div5 div6 div7 div8 div9 div10 div11 div12 div13 div14 div15");
-							//$(this).addClass("empty").removeClass("div1 div2 div3 div4 div5 div6 div7 div8 div9 div10 div11 div12 div13 div14 div15");
+							$("#div"+adjacents[0]).addClass("empty").removeClass("div1 div2 div3 div4 div5 div6 div7 div8 div9 div10 div11 div12 div13 div14 div15")
+							updateText();
+							showWinningScreen (checkIfUserWon());
 						}
 					}
 				}
 			}
 		);
-
-
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
 );
 
-function getAdjacentClass( item )
+
+/*
+ * Tries to swap tiles by clicking on a random square in the grid.  If the move is legitimate, then the swap wil
+ * occur, otherwise it will not.  Most of the time (i.e. probably 70-80%) the click will not make the swap.  This is why
+ * we call this function many times in order to compensate for the high probability of inaction.
+ */
+function tryToSwapTiles()
 {
-	
+	var number = Math.floor(Math.random() * 16) + 1; // 1 to 16
+	console.log("Clicked on grd " + number);
+	$("#div"+number).trigger("click");
+	return;
 }
 
 function isCellNextToEmpty(  cell )
@@ -125,13 +134,13 @@ function isCellNextToEmpty(  cell )
 
 
 	var coords = [];
-	if ( clickedCellId == emptyCellId + 1 )
+	if ( clickedCellId == emptyCellId + 1 && clickedCellId % 4 != 1)
 	{
 		coords.push(clickedCellId);
 		coords.push(emptyCellId);	
 		return coords;
 	}
-	else if ( clickedCellId == emptyCellId - 1 )
+	else if ( clickedCellId == emptyCellId - 1 && clickedCellId % 4 != 0 )
 	{
 		coords.push(clickedCellId);
 		coords.push(emptyCellId);	
@@ -150,10 +159,59 @@ function isCellNextToEmpty(  cell )
 		return coords;
 	}
 	return coords;
-
-	//if (cell.hasClass("empty") )
-	//	return false;
-		//alert('clicked onm empty one');
-	//else if (
 		
+}
+
+
+/* 
+ * Redraw the hint text (i.e. call this after tile has been moved)
+ * If the hintsOn global variable is set, then the hint text will be set (otherwise it will be empty)
+ */
+function updateText() {	
+
+	for (var i=1; i<=16; i++)
+	{			
+		var text = $("#div"+i).attr('class').replace("div","").replace("empty", "empty (16)");
+		if (!hintsOn)
+			text = "";
+		$("#div"+i).text( text );
+	}
+}
+
+/*
+ * if any of the divs ID do not match the div classes then the user has not won
+ * (Note that div id 16 should match to empty)
+ */
+function checkIfUserWon() {	
+	for (var i=1; i<=15; i++)
+	{			
+		var divClass = $("#div"+i).attr('class');
+		if (divClass !== "div"+i)
+		{
+			return false;
+		}
+	}
+	if ( $("#div16").attr('class') !== 'empty' )
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+/*
+ * Shows a congragulatory message if true, otherwise message is empty
+ */
+function showWinningScreen( show )
+{
+	if (show)
+	{
+		console.log("you won");
+		$("#divwin").text("Congratulations!  You won!");	
+	}
+	else
+	{
+		$("#divwin").text("");
+	}
 }
