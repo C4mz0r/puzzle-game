96dbd5b125f3cfb678b15e655da155ed20cabca1
C4mz0r <aut0x@hotmail.com>
2015-08-15 10:09:34 -0600

    cleanup of code and css

--- game.js
+++ game.js
@@ -55,111 +55,41 @@ $(document).ready (
  */
 function tryToSwapTiles()
 {
-	var number = Math.floor(Math.random() * 16) + 1; // 1 to 16
-	console.log("Clicked on grd " + number);
+	var number = Math.floor(Math.random() * 16) + 1; // 1 to 16	
 	$("#div"+number).trigger("click");
 	return;
 }
 
+/*
+ * Return an array with the clicked cell # (i.e. 1 to 16) and the empty cell # if they're adjacent.
+ * Otherwise the array will be empty.
+ */
 function isCellNextToEmpty(  cell )
 {
-	var clickedCellId = null;
-	if (cell.is("#div1"))
-		clickedCellId = 1;
-	else if (cell.is("#div2"))
-		clickedCellId = 2;
-	else if (cell.is("#div3"))
-		clickedCellId = 3;
-	else if (cell.is("#div4"))
-		clickedCellId = 4;
-	else if (cell.is("#div5"))
-		clickedCellId = 5;	
-	else if (cell.is("#div6"))
-		clickedCellId = 6;
-	else if (cell.is("#div7"))
-		clickedCellId = 7;
-	else if (cell.is("#div8"))
-		clickedCellId = 8;
-	else if (cell.is("#div9"))
-		clickedCellId = 9;
-	else if (cell.is("#div10"))
-		clickedCellId = 10;
-	else if (cell.is("#div11"))
-		clickedCellId = 11;
-	else if (cell.is("#div12"))
-		clickedCellId = 12;
-	else if (cell.is("#div13"))
-		clickedCellId = 13;
-	else if (cell.is("#div14"))
-		clickedCellId = 14;
-	else if (cell.is("#div15"))
-		clickedCellId = 15;
-	else if (cell.is("#div16"))
-		clickedCellId = 16;
-
-	var emptyCellId = null;
-	if ($("#div1").is(".empty"))
-		emptyCellId = 1;
-	else if ($("#div2").is(".empty"))
-		emptyCellId = 2;
-	else if ($("#div3").is(".empty"))
-		emptyCellId = 3;
-	else if ($("#div4").is(".empty"))
-		emptyCellId = 4;
-	else if ($("#div5").is(".empty"))
-		emptyCellId = 5;
-	else if ($("#div6").is(".empty"))
-		emptyCellId = 6;
-	else if ($("#div7").is(".empty"))
-		emptyCellId = 7;
-	else if ($("#div8").is(".empty"))
-		emptyCellId = 8;
-	else if ($("#div9").is(".empty"))
-		emptyCellId = 9;
-	else if ($("#div10").is(".empty"))
-		emptyCellId = 10;
-	else if ($("#div11").is(".empty"))
-		emptyCellId = 11;
-	else if ($("#div12").is(".empty"))
-		emptyCellId = 12;
-	else if ($("#div13").is(".empty"))
-		emptyCellId = 13;
-	else if ($("#div14").is(".empty"))
-		emptyCellId = 14;
-	else if ($("#div15").is(".empty"))
-		emptyCellId = 15;
-	else if ($("#div16").is(".empty"))
-		emptyCellId = 16;
-	console.log("Empty one is " + emptyCellId);
-
-
+	var clickedCellId = Number.parseInt(cell.attr('id').replace("div","")); // will be something like div15, need to remove "div" and make it into an integer
+	var emptyCellId = Number.parseInt( $(".empty").attr('id').replace("div","") ); // will be something like div16, need to remove "div" and make it into an integer
 	var coords = [];
 	if ( clickedCellId == emptyCellId + 1 && clickedCellId % 4 != 1)
 	{
 		coords.push(clickedCellId);
 		coords.push(emptyCellId);	
-		return coords;
 	}
 	else if ( clickedCellId == emptyCellId - 1 && clickedCellId % 4 != 0 )
 	{
 		coords.push(clickedCellId);
 		coords.push(emptyCellId);	
-		return coords;
 	}
 	else if ( clickedCellId == emptyCellId + 4 )
 	{
 		coords.push(clickedCellId);
 		coords.push(emptyCellId);	
-		return coords;
 	}
 	else if ( clickedCellId == emptyCellId - 4 )
 	{
 		coords.push(clickedCellId);
 		coords.push(emptyCellId);	
-		return coords;
 	}
-	return coords;
-		
+	return coords;	
 }
 
 
@@ -207,7 +137,6 @@ function showWinningScreen( show )
 {
 	if (show)
 	{
-		console.log("you won");
 		$("#divwin").text("Congratulations!  You won!");	
 	}
 	else
